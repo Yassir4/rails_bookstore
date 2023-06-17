50a0c99e84b968d0a76c377b8612d53b893f9629
Yassir Hartani <hartaniyassir@gmail.com>
2023-06-17 18:41:06 +0100

    add books model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_144340) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_174026) do
   create_table "authors", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "name", default: "", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_144340) do
     t.index ["email"], name: "index_authors_on_email", unique: true
     t.index ["jti"], name: "index_authors_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_144340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "authors"
 end
